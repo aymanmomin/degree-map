a0d55987d2bc0d62ac9b4929a01dfb0206daf930
ChristianN517 <98929417+ChristianN517@users.noreply.github.com>
2024-11-29 12:50:08 -0700

    Removed back end stuff

--- src/db/471Backend-chris/db.js
+++
@@ -1,8 +0,0 @@
-const { Sequelize } = require('sequelize');
-
-const sequelize = new Sequelize({
-    dialect: 'sqlite',
-    storage: './database.sqlite', //file-based storage
-});
-
-module.exports = sequelize;
