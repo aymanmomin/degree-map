1944547b85f75a74ea3c3831c1b361a7c1dd6ace
Ayman M <63975056+aymanmomin@users.noreply.github.com>
2024-12-03 10:31:35 -0700

    creating all the forms.js and linking it

--- src/App.js
+++ src/App.js
@@ -4,8 +4,11 @@ import Navbar from "./components/Navbar";
 import Profile from "./components/Profile";
 
 import AdminBar from "./admin-components/Navbar";
-// import AdminDashboard from "./admin-components/Dashboard";
+import AdminDashboard from "./admin-components/Dashboard";
 import ManageProgramForm from "./admin-components/ManageProgramForm";
+import ManageCoursesForm from "./admin-components/ManageCoursesForm";
+import ManageInstructorsForm from "./admin-components/ManageInstructorsForm";
+import ManageStudentsForm from "./admin-components/ManageStudentsForm";
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { useState } from "react";
@@ -21,8 +24,14 @@ function App() {
           {userType === "admin" ? (
             <>
               {/* Admin Routes */}
-              {/* <Route path="/admin-dashboard" element={<AdminDashboard />} /> */}
+              <Route path="/admin-dashboard" element={<AdminDashboard />} />
               <Route path="/manage-programs" element={<ManageProgramForm />} />
+              <Route path="/manage-courses" element={<ManageCoursesForm />} />
+              <Route
+                path="/manage-instructors"
+                element={<ManageInstructorsForm />}
+              />
+              <Route path="/manage-students" element={<ManageStudentsForm />} />
             </>
           ) : (
             <>
