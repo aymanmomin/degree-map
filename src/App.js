9dca536f30875f37a839b78109e8f0390e1fc30b
Ayman M <63975056+aymanmomin@users.noreply.github.com>
2024-12-02 23:28:25 -0700

    adding different user preview

--- src/App.js
+++ src/App.js
@@ -2,15 +2,37 @@ import "./App.css";
 import ManageInterests from "./components/ManageInterests";
 import Navbar from "./components/Navbar";
 import Profile from "./components/Profile";
+
+import AdminBar from "./admin-components/Navbar";
+import AdminDashboard from "./admin-components/Dashboard";
+
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { useState } from "react";
+
 function App() {
+  const [userType, setUserType] = useState("admin"); // Change to 'admin' for admin views
+
   return (
     <>
       <Router>
-        <Navbar />
+        {userType === "admin" ? <AdminBar /> : <Navbar />}
         <Routes>
-          <Route path="/view-profile" element={<Profile />} />
-          <Route path="/manage-interests" element={<ManageInterests />} />
+          {userType === "admin" ? (
+            <>
+              {/* Admin Routes */}
+              <Route path="/admin-dashboard" element={<AdminDashboard />} />
+              <Route
+                path="/manage-interests"
+                element={<ManageInterests />} // Admin can also manage interests
+              />
+            </>
+          ) : (
+            <>
+              {/* Student Routes */}
+              <Route path="/view-profile" element={<Profile />} />
+              <Route path="/manage-interests" element={<ManageInterests />} />
+            </>
+          )}
         </Routes>
       </Router>
     </>
